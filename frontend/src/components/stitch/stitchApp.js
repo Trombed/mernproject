fb3be1892330bc44a2ca732af8b1fe1639a1e9f0
byfebeef <jimmykang89@gmail.com>
2020-03-18 14:18:55 -0700

    making stitch container

---
+++ frontend/src/components/stitch/stitchApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class StitchApp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default StitchApp;

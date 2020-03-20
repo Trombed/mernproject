7be80c28e9e5dccc07ff76ccfb114f9681fd0558
irynamcb <iryna.mcbride@gmail.com>
2020-03-19 21:23:07 -0700

    Add Like model

---
+++ models/Like.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LikeSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    meme: {
+        type: Schema.Types.ObjectId,
+        ref: 'Meme'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Like = mongoose.model('Like', MemeSchema);
+
+module.exports = Like;

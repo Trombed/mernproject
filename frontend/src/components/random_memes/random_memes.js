db1d7f3665b140d3f2c9455f7b0c8f3132eae44e
irynamcb <iryna.mcbride@gmail.com>
2020-03-21 15:25:11 -0700

    Add loading more items

--- frontend/src/components/random_memes/random_memes.js
+++ frontend/src/components/random_memes/random_memes.js
@@ -6,17 +6,35 @@ class RandomMemes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            memes: []
+            memes: [],
+            loading: false,
+            newMemes: [],
+            page: 0
         }
     }
 
     componentDidMount() {
-        this.getMemes();
+        // this.getMemes();
+
+        this.refs.iScroll.addEventListener("scroll", () => {
+            if (this.refs.iScroll.scrollTop + this.refs.iScroll.clientHeight >= this.refs.iScroll.scrollHeight) {
+                this.getMemes();
+            }
+        });
     }
 
     async getMemes() {
+        this.setState({ loading: true });
+
+        let memes = this.state.memes;
+        let newMemes = this.state.newMemes;
+        let page = this.state.page;
+
+        if (newMemes.length === 0) {
+
+        page +=1;
         const response = await fetch(
-            `https://api.imgur.com/3/gallery/hot/viral/1`, {
+            `https://api.imgur.com/3/gallery/hot/viral/${page}`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
@@ -25,7 +43,13 @@ class RandomMemes extends React.Component {
             }
         );
         const result = await response.json();
-        this.setState({memes: result.data})
+
+        newMemes = result.data.filter(meme => meme.images_count > 0 && meme.images[0].type.startsWith('image/'));
+    }
+        memes = memes.concat(newMemes.slice(0,10));
+        newMemes = newMemes.slice(10);
+
+        this.setState({memes: memes, newMemes: newMemes, page: page, loading: false})
 
     }
     render() {
@@ -34,11 +58,10 @@ class RandomMemes extends React.Component {
             
             <div className="memes"> 
                 {
-                
                 this.state.memes
-                            .filter(meme => meme.images_count > 0 && meme.images[0].type.startsWith('image/'))
-                            .sort(() => 0.5 - Math.random())
-                            .slice(0, 10)
+                            // .filter(meme => meme.images_count > 0 && meme.images[0].type.startsWith('image/'))
+                            // .sort(() => 0.5 - Math.random())
+                            // .slice(0, 10)
                             .map(meme => (
                                 <RandomMeme 
                                 key={meme.id}
@@ -46,6 +69,7 @@ class RandomMemes extends React.Component {
                                 image={meme.images[0].link}/>
                                 ))
                 }
+                {this.state.loading ? <p className="loading"> loading More Items..</p> : ""}
             </div>
         )
     }

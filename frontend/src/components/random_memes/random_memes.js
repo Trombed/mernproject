87397695f51b11fc5aa8916405a1d890f0a1e449
irynamcb <iryna.mcbride@gmail.com>
2020-03-20 23:19:14 -0700

    Start on RandomMemes component

--- frontend/src/components/random_memes/random_memes.js
+++ frontend/src/components/random_memes/random_memes.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import RandromMeme from './random_meme';
 
 class RandomMemes extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
+    componentDidMount() {
+
+    }
+
+    async getMemes() {
+        const response = await fetch(
+            `https://api.imgur.com/3/gallery/hot/viral/1`
+        );
+        const data = await response.json();
+
+    }
 
 }
 

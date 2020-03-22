7730c549f370008fc55245e9467f87941beb093c
Eric Ho <eric.h924@gmail.com>
2020-03-22 01:17:16 -0700

    prepare for heroku push

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     })
-// }
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 mongoose
     .connect(db, { useNewUrlParser: true })
